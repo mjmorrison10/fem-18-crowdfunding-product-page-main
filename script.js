dc8081e546ef8ea2185023c47a73a92a786e5a2a
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-11-10 18:42:08 -0800

    modal
    secondary modal

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const Navbar = document.querySelector(".nav-bar");
 const Navmenu = document.querySelector(".nav-menu");
+const ButtonProject = document.querySelector(".btn-project");
+const Pledge = document.querySelector(".btn-pledge");
+const Modal = document.querySelector(".modal");
 
 Navbar.addEventListener("click", () => {
   console.log(Navbar.children[0]);
@@ -9,3 +12,18 @@ Navbar.addEventListener("click", () => {
   Navmenu.parentNode.style.display =
     Navmenu.parentNode.style.display == "flex" ? "none" : "flex";
 });
+
+ButtonProject.addEventListener("click", () => {
+  Modal.style.display = Modal.style.display == "flex" ? "none" : "flex";
+  setTimeout(() => {
+    console.log("test");
+    Modal.style.display = "none";
+  }, 10000);
+});
+
+Pledge.addEventListener("click", () => {
+  Modal.style.display = "none";
+});
+
+
+
